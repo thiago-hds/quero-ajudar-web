f9c3d4a35c1aad4b092682f4bb50549dab37b4a6
Thiago Santos <s.thiagoh@gmail.com>
2021-11-25 11:32:02 -0300

    refactor volunteers views

--- resources/views/volunteers/edit.blade.php
+++ resources/views/volunteers/edit.blade.php
@@ -1,180 +1,145 @@
-@extends('adminlte::page')
+@php
+use App\Enums\ProfileType;
+use App\Enums\StatusType;
+@endphp
 
-@section('title', (isset($volunteer)? 'Editar' : 'Novo') . ' Voluntário')
+@extends('layout.edit', [
+'model' => $volunteer ?? null,
+'title' => sprintf("%s %s", isset($volunteer) ? 'Editar' : 'Novo', "Voluntário"),
+'action' => isset($volunteer) ? route('volunteers.update', $volunteer->id) : route('volunteers.store'),
+'cancelUrl' => route('volunteers.index')
+])
 
-@section('content_header')
-    <h1 class="m-0 text-dark">{{ (isset($volunteer)? 'Editar' : 'Novo') . ' Voluntário' }}</h1>
-@stop
+@section('fields')
 
-@section('content')    
     <div class="row">
-        <div class="col-12">
-        
-            <div class="card">
-                <!-- form start -->
-                <form role="form" method="post" action="{{ isset($volunteer->user_id)? route('volunteers.update', $volunteer->user_id) : route('volunteers.store') }}">
-                    @if(isset($volunteer))
-                        @method('PATCH') 
-                    @endif
-                    @csrf
-                    <div class="card-body">
+        {{-- first_name --}}
+        <x-adminlte-input
+            type="text"
+            name="first_name"
+            label="Nome"
+            fgroup-class="col-sm-4"
+            value="{{ old('first_name', $volunteer->user->first_name ?? '') }}"
+        />
 
-                        <div class="row">
-                            <!-- first_name -->
-                            <div class="col-sm-4">
-                                <div class="form-group">
-                                    <label for="first_name">Nome</label>
-                                    <input type="text" class="form-control @error('first_name') is-invalid @enderror" name="first_name" value="{{ old('first_name', isset($volunteer->user->first_name) ? $volunteer->user->first_name : null) }}">
-                                    @error('first_name')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>  
-                            </div>
-                            
-                            <!-- last_name -->
-                            <div class="col-sm-4">
-                                <div class="form-group">
-                                    <label for="last_name">Sobrenome</label>
-                                    <input type="text" class="form-control @error('last_name') is-invalid @enderror" name="last_name" value="{{ old('last_name', isset($volunteer->user->last_name) ? $volunteer->user->last_name : null) }}">
-                                    @error('last_name')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>  
-                            </div>
+        {{-- last_name --}}
+        <x-adminlte-input
+            type="text"
+            name="last_name"
+            label="Sobrenome"
+            fgroup-class="col-sm-4"
+            value="{{ old('last_name', $volunteer->user->last_name ?? '') }}"
+        />
 
-                            <!-- date_of_birth -->
-                            <div class="col-sm-4">
-                                <div class="form-group">
-                                    <label for="date_of_birth">Data de Nascimento</label>
-                                    <div class="input-group">
-                                        <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
-                                        </div>
-                                        <input type="text" class="form-control date-input @error('date_of_birth') is-invalid @enderror" placeholder="dd/mm/aaaa" name="date_of_birth" value="{{ old('date_of_birth', isset($volunteer->user->date_of_birth) ? $volunteer->user->date_of_birth : null) }}">
-                                        @error('date_of_birth')
-                                            <div class="invalid-feedback">{{ $message }}</div>
-                                        @enderror
-                                    </div>
-                                </div> 
-                            </div>
-                        </div>
+        {{-- date_of_birth --}}
+        <x-adminlte-input
+            type="text"
+            name="date_of_birth"
+            label="Data de Nascimento"
+            placeholder="dd/mm/aaaa"
+            fgroup-class="col-md-4"
+            value="{{ old('date_of_birth', $volunteer->user->date_of_birth ?? '') }}"
+        >
 
-                        <div class="row">
-                            <!-- causes -->
-                            <div class="col-sm-6">
-                                <div class="form-group">
-                                    <label for="causes">Causas</label>
-                                    <select class="form-control select2  @error('causes') is-invalid @enderror" multiple="multiple" data-placeholder="Selecione uma ou mais causas" style="width: 100%;" name="causes[]">
-                                        <option></option>
-                                        @foreach($causes as $cause)
-                                            <option value="{{ $cause->id }}" {{ (in_array($cause->id, old('causes', isset($volunteer->causes)? $volunteer->causes->pluck('id')->all() : array())))? 'selected' : '' }} >
-                                                {{ $cause->name }}
-                                            </option>
-                                        @endforeach
-                                    </select>
+            <x-slot name="prependSlot">
+                <span class="input-group-text">
+                    <i class="far fa-calendar-alt"></i>
+                </span>
+            </x-slot>
+        </x-adminlte-input>
 
-                                    @error('causes')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
-                            </div>
-                            <!-- skills -->
-                            <div class="col-sm-6">
-                                <div class="form-group">
-                                    <label for="skills">Habilidades</label>
-                                    <select class="form-control select2  @error('skills') is-invalid @enderror" multiple="multiple" data-placeholder="Selecione uma ou mais habilidades" style="width: 100%;" name="skills[]">
-                                        <option></option>
-                                        @foreach($skills as $skill)
-                                            <option value="{{ $skill->id }}" {{ (in_array($skill->id, old('skills', isset($volunteer->skills)? $volunteer->skills->pluck('id')->all() : array())))? 'selected' : '' }} >
-                                                {{ $skill->name }}
-                                            </option>
-                                        @endforeach
-                                    </select>
+    </div>
 
-                                    @error('skills')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
-                            </div>
-                        </div>
+    <div class="row">
+        {{-- causes --}}
+        <x-causes-select fgroup-class="col-sm-6" />
 
-                        <!-- email -->
-                        <div class="form-group">
-                            <label for="email">E-mail</label>
-                            <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email', isset($volunteer->user->email) ? $volunteer->user->email : null) }}">
-                            @error('email')
-                                <div class="invalid-feedback">{{ $message }}</div>
-                            @enderror
-                        </div>
+        {{-- skills --}}
+        <x-skills-select fgroup-class="col-sm-6" />
+    </div>
 
-                        <div class="row">
-                            <!-- password -->
-                            <div class="col-sm-6">
-                                <div class="form-group">
-                                    <label for="password">Senha</label>
-                                    <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" value="{{ old('password', isset($volunteer->user->password) ? $volunteer->user->password : null) }}">
-                                    @error('password')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
-                            </div>
+    <div class="row">
 
-                            <!-- passwordconfirm -->
-                            <div class="col-sm-6">
-                                <div class="form-group">
-                                    <label for="password_confirm">Confirmação de Senha</label>
-                                    <input type="password" class="form-control @error('password_confirm') is-invalid @enderror" name="password_confirm" value="{{ old('password_confirm', isset($volunteer->user->password) ? $volunteer->user->password : null) }}">
-                                    @error('password_confirm')
-                                        <div class="invalid-feedback">{{ $message }}</div>
-                                    @enderror
-                                </div>
-                            </div>
-                        </div>
+        {{-- email --}}
+        <x-email-input
+            fgroup-class="col-md-4"
+            value="{{ old('email', $volunteer->user->email ?? '') }}"
+        />
 
-                        <!-- status -->
-                        <div class="form-group">
-                            <label for="status">Status</label>
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio"
-                                    name="status" value="1" {{ old('status', isset($volunteer->user->status)? $volunteer->user->status : null) == \App\Enums\StatusType::INACTIVE? '' : 'checked' }}>
-                                <label class="form-check-label">Ativo</label>
-                            </div>
+        {{-- password --}}
+        <x-adminlte-input
+            type="password"
+            name="password"
+            label="Senha"
+            fgroup-class="col-md-4"
+            value="{{ $volunteer->user->password ?? '' }}"
+        >
 
-                            <div class="form-check">
-                                <input class="form-check-input" type="radio"
-                                    name="status" value="0" {{ old('status', isset($volunteer->user->status)? $volunteer->user->status : null) == \App\Enums\StatusType::INACTIVE? 'checked' : '' }}>
-                                <label class="form-check-label">Inativo</label>
-                            </div>
-                            
+            <x-slot name="prependSlot">
+                <span class="input-group-text">
+                    <i class="fa fa-key"></i>
+                </span>
+            </x-slot>
 
-                            @error('status')
-                                {{ $message }} 
-                                <div class="invalid-feedback">{{ $message }}</div>
-                            @enderror
-                        </div>
-                    </div>
-                    <!-- /.card-body -->
+        </x-adminlte-input>
 
-                    <div class="card-footer">
-                    <button type="submit" class="btn btn-success float-right">
-                        <i class="fas fa-save"></i>  Salvar
-                    </button>
-                    <a class="btn btn-danger" href="{{ route('volunteers.index')}}">
-                        <i class="fas fa-arrow-left"></i>  Cancelar
-                    </a>    
-                    </div>
-                </form>
+        {{-- password confirmation --}}
+        <x-adminlte-input
+            type="password"
+            name="password_confirm"
+            label="Confirmação de Senha"
+            fgroup-class="col-md-4"
+            placeholder="Repita a senha"
+            value="{{ $volunteer->user->password ?? '' }}"
+        >
 
-            </div>
-        </div>
+            <x-slot name="prependSlot">
+                <span class="input-group-text">
+                    <i class="fa fa-key"></i>
+                </span>
+            </x-slot>
+
+        </x-adminlte-input>
+    </div>
+
+    <!-- status -->
+    <div class="row">
+        <x-form-group label="Status">
+            @php
+
+                $isActive = old('status', $volunteer->user->status ?? StatusType::ACTIVE) == StatusType::ACTIVE;
+            @endphp
+
+            <x-radio
+                name="status"
+                label="Ativo"
+                value="{{ StatusType::ACTIVE }}"
+                checked="{{ $isActive }}"
+            >
+            </x-radio>
+
+            <x-radio
+                name="status"
+                label="Inativo"
+                value="{{ StatusType::INACTIVE }}"
+                checked="{{ !$isActive }}"
+            >
+            </x-radio>
+
+            @error('status')
+                <div class="invalid-feedback">{{ $message }}</div>
+            @enderror
+        </x-form-group>
     </div>
 @stop
 
 @section('css')
-    <link rel="stylesheet" href="{{ asset('/css/panel.css') }}">
+    <link
+        rel="stylesheet"
+        href="{{ asset('/css/panel.css') }}"
+    >
 @stop
 
 @section('js')
     <script src="{{ asset('/js/panel.js') }}"></script><s></s>
 @stop
-
