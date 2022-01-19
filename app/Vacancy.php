922763b44293b4cc511b260102f68993219844b7
Thiago Santos <s.thiagoh@gmail.com>
2022-01-19 16:34:52 -0300

    refactor vacancy controller

--- app/Vacancy.php
+++ app/Vacancy.php
@@ -8,6 +8,7 @@ use App\Enums\RecurrenceType;
 use App\Enums\UnitPerPeriodType;
 use Illuminate\Database\Eloquent\Model;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 
 class Vacancy extends Model
 {
@@ -31,13 +32,57 @@ class Vacancy extends Model
         'location_type'
     ];
 
+    public function scopeFilter($query, $filters)
+    {
+        $query->when($filters['name'] ?? false, function ($query, $name) {
+            $query->where('name', 'like', "%{$name}%");
+        });
+
+        $query->when($filters['status'] ?? false, function ($query, $status) {
+            $query->where('status', $status);
+        });
+
+        $query->when($filters['type'] ?? false, function ($query, $type) {
+            $query->where('type', $type);
+        });
+
+        $query->when($filters['organization_id'] ?? false, function ($query, $organization_id) {
+            $query->where('organization_id', $organization_id);
+        });
+
+        $query->when($filters['cause_id'] ?? false, function ($query, $causeId) {
+            $query->whereHas('causes', function ($query) use ($causeId) {
+                $query->where('id', $causeId);
+            });
+        });
+
+        $query->when($filters['skill_id'] ?? false, function ($query, $skillId) {
+            $query->whereHas('skills', function ($query) use ($skillId) {
+                $query->where('id', $skillId);
+            });
+        });
+
+        $query->when($filters['address_city'] ?? false, function ($query, $cityId) {
+            $query->whereHas('address.city', function ($query) use ($cityId) {
+                $query->where('id', $cityId);
+            });
+        });
+
+        $query->when($filters['address_state'] ?? false, function ($query, $stateAbbr) {
+            $query->whereHas('address.city.state', function ($query) use ($stateAbbr) {
+                $query->where('abbr', $stateAbbr);
+            });
+        });
+    }
+
     /**
-     * Get the organization of the user.
+     * Get the vacancy's organization.
      */
     public function organization()
     {
         return $this->belongsTo('App\Organization');
     }
+
 
     public function address()
     {
@@ -66,7 +111,7 @@ class Vacancy extends Model
 
     public function getPromotionStartDateAttribute($value)
     {
-        if($value !== null){
+        if ($value !== null) {
             return Carbon::parse($value)->format('d/m/Y');
         }
         return null;
@@ -75,15 +120,14 @@ class Vacancy extends Model
     public function setPromotionStartDateAttribute($value)
     {
         $this->attributes['promotion_start_date'] = null;
-        if($value !== null){
+        if ($value !== null) {
             $this->attributes['promotion_start_date'] = Carbon::createFromFormat('d/m/Y', $value)->toDateString();
         }
-
     }
 
     public function getPromotionEndDateAttribute($value)
     {
-        if($value !== null){
+        if ($value !== null) {
             return Carbon::parse($value)->format('d/m/Y');
         }
         return null;
@@ -92,16 +136,15 @@ class Vacancy extends Model
     public function setPromotionEndDateAttribute($value)
     {
         $this->attributes['promotion_end_date'] = null;
-        if($value !== null){
+        if ($value !== null) {
             $this->attributes['promotion_end_date'] = Carbon::createFromFormat('d/m/Y', $value)->toDateString();
         }
-
     }
 
     public function setDateAttribute($value)
     {
         $this->attributes['date'] = null;
-        if($value !== null){
+        if ($value !== null) {
             $this->attributes['date'] = Carbon::createFromFormat('d/m/Y', $value)->toDateTimeString();
         }
     }
@@ -109,14 +152,14 @@ class Vacancy extends Model
     public function setTimeAttribute($value)
     {
         $this->attributes['time'] = null;
-        if($value !== null){
+        if ($value !== null) {
             $this->attributes['time'] = Carbon::createFromFormat('H:i', $value)->toDateTimeString();
         }
     }
 
     public function getDateAttribute()
     {
-        if($this->attributes['date'] !== null){
+        if ($this->attributes['date'] !== null) {
             return Carbon::parse($this->attributes['date'])->format('d/m/Y');
         }
         return null;
@@ -124,7 +167,7 @@ class Vacancy extends Model
 
     public function getTimeAttribute()
     {
-        if($this->attributes['time'] !== null){
+        if ($this->attributes['time'] !== null) {
             return Carbon::parse($this->attributes['time'])->format('H:i');
         }
         return null;
@@ -132,94 +175,84 @@ class Vacancy extends Model
 
     public function getFormattedFrequency()
     {
-        if($this->type == RecurrenceType::RECURRENT){
-            if($this->amount_per_period == null){
+        if ($this->type == RecurrenceType::RECURRENT) {
+            if ($this->amount_per_period == null) {
                 return "Vaga recorrente";
-            }
-            else{
+            } else {
                 $amount = $this->amount_per_period;
-                if($this->unit_per_period == UnitPerPeriodType::HOURS){
-                    $unit = $amount == 1? "hora" : "horas";
-                }
-                else if($this->unit_per_period == UnitPerPeriodType::DAYS){
-                    $unit = $amount == 1? "dia" : "dias";
+                if ($this->unit_per_period == UnitPerPeriodType::HOURS) {
+                    $unit = $amount == 1 ? "hora" : "horas";
+                } elseif ($this->unit_per_period == UnitPerPeriodType::DAYS) {
+                    $unit = $amount == 1 ? "dia" : "dias";
                 }
 
-                if($this->periodicity == PeriodicityType::DAILY){
+                if ($this->periodicity == PeriodicityType::DAILY) {
                     $period = "diárias";
-                }
-                else if($this->periodicity == PeriodicityType::WEEKLY){
+                } elseif ($this->periodicity == PeriodicityType::WEEKLY) {
                     $period = "semanais";
-                }
-                else if($this->periodicity == PeriodicityType::MONTHLY){
+                } elseif ($this->periodicity == PeriodicityType::MONTHLY) {
                     $period = "mensais";
                 }
 
                 return sprintf("%d %s %s", $amount, $unit, $period);
             }
-        }
-        else if($this->type == RecurrenceType::UNIQUE_EVENT){
-            if($this->date){
+        } elseif ($this->type == RecurrenceType::UNIQUE_EVENT) {
+            if ($this->date) {
                 return sprintf("%s %s", $this->date, $this-> time);
-            }
-            else{
+            } else {
                 return "À combinar";
             }
-        }
-        else{
+        } else {
             return null;
         }
     }
 
-    public function getFormattedDate(){
-        if($this->type == RecurrenceType::UNIQUE_EVENT){
-            if($this->date == null){
+    public function getFormattedDate()
+    {
+        if ($this->type == RecurrenceType::UNIQUE_EVENT) {
+            if ($this->date == null) {
                 return "À combinar";
-            }
-            else{
+            } else {
                 return $this->date;
             }
+        } else {
+            return "Vaga recorrente";
         }
-	else{
-		return "Vaga recorrente";
-	}
     }
 
-    public function getFormattedTime(){
-        if($this->time == null){
+    public function getFormattedTime()
+    {
+        if ($this->time == null) {
             return "À combinar";
-        }
-        else{
+        } else {
             return $this->time;
         }
     }
 
-    public function getFormattedLocation($complete = true){
-        if($this->location_type == LocationType::REMOTE){
+    public function getFormattedLocation($complete = true)
+    {
+        if ($this->location_type == LocationType::REMOTE) {
             return "Remoto";
-        }
-        else if($this->location_type == LocationType::NEGOTIABLE){
+        } elseif ($this->location_type == LocationType::NEGOTIABLE) {
             return "À combinar";
-        }
-        else if($this->location_type == LocationType::ORGANIZATION_ADDRESS ){
-            if(($organization = Organization::find($this->organization_id)) &&
-                isset($organization->address)){
-                    if($complete){
-                        return $organization->address->getFormattedAddress();
-                    }
-                    else{
-                        return $organization->address->getLocation();
-                    }
+        } elseif ($this->location_type == LocationType::ORGANIZATION_ADDRESS) {
+            if (
+                ($organization = Organization::find($this->organization_id)) &&
+                isset($organization->address)
+            ) {
+                if ($complete) {
+                    return $organization->address->getFormattedAddress();
+                } else {
+                    return $organization->address->getLocation();
+                }
             }
-        }
-        else if($this->address != null){
-            if($complete){
+        } elseif ($this->address != null) {
+            if ($complete) {
                 return $this->address->getFormattedAddress();
-            }
-            else{
+            } else {
                 return $this->address->getLocation();
             }
         }
         return null;
-    }       
+    }
 }
